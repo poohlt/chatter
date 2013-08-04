d2ea43a48f3ce2d67d74d5d4aa892e785da32d91
Tianyu Liu <poohlty@sina.com>
2013-08-04 09:56:16 -0400

    initial commit. Basic socket function working, very basic app structure working

---
+++ app.js
@@ -0,0 +1,39 @@
+var socket = require('socket.io');
+var express = require('express');
+var http = require('http');
+var app = express();
+
+app.use(express.static(__dirname + '/static'));
+
+// app.get('/',function(req,res){
+//     res.render('index.html');
+// });
+
+var server = http.createServer(app);
+var io = socket.listen(server);
+var messages = [];
+
+var pushMessage = function(name, data){
+    messages.push({name: name, data: data});
+};
+
+io.sockets.on('connection', function(client){
+    console.log("client conneted..");
+
+    client.on('join', function(name){
+        client.set('nickname', name);
+        messages.forEach(function(message){
+            client.emit('messages', message.name +': '+ message.data);
+        });
+    });
+
+    client.on('messages', function(data){
+        client.get('nickname', function(err, name){
+            client.broadcast.emit('messages', name +': '+ data);
+            pushMessage(name, data);
+            console.log(name +': '+ data);
+        });
+    });
+});
+
+server.listen(8080);
---
+++ static/coffee/chatter.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    var server = io.connect('http://localhost:8080');
+    var insertMessage = function(message){
+        $("#message-history").append(message);
+    };
+
+    server.on('connect', function(data){
+        nickname = prompt("What's your nickname?");
+        server.emit('join',nickname);
+    });
+
+    server.on('messages', function(data){
+        console.log('This is called');
+        insertMessage(data + "<br/>");
+    });
+
+    $("#chat-form").submit(function(){
+        var message = $('#chat-input').val();
+        insertMessage("Me:" + message + "<br/>");
+        server.emit('messages',message);
+        return false;
+    });
+});
